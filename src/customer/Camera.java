6c878943ac224791e0463bc5df40c716e70343f7
luongminh2111 <79582356+luongminh2111@users.noreply.github.com>
2021-05-19 13:32:38 +0700

    Add files via upload

--- src/customer/Camera.java
+++ src/customer/Camera.java
@@ -26,103 +26,56 @@ public class Camera extends Cordination {
         this.scope = scope;
     }
 
-    public Camera(double x, double y, double z, double sightCorner, double scope) {
+    public Camera(double x, double y, double z, double sightCorner) {
         super(x, y, z);
         this.sightCorner = sightCorner;
-        this.scope = scope;
+
     }
-    //Khoi tao cac thong so cho camera
-    public boolean checkCamare(Characteristics room, String s) {
-        List<Double> list = new ArrayList<>();
-//        Scanner keyboard = new Scanner(System.in);
-//        double xCor, yCor, zCor, tmpScope, tmpSightCorner;
-//        String s = keyboard.nextLine();
-        s = s.trim();
-        int i = 0 ;
-        if (s.charAt(0) == '('){
-            while ( i < s.length()){
-                int j = i ;
-                while (s.charAt(j) != ')'){
-//                    System.out.println(s.charAt(j));
-                    if (s.charAt(j) == ',' && s.charAt(j+1) == ' '){
-//                        System.out.println(j);
-                        String x =  s.substring(i+1,j);
-//                        System.out.println(x);
-                        double so = Double.parseDouble(x);
-                        System.out.println(so);
-                        if (so < 0) {
-                            System.out.println("Error, the value must be positive .");
-                            return false;
-                        } else {
-                            list.add(so);
-                            i = j + 1;
-                        }
-                    }
+    private Cordination pointA;
+    private Cordination pointB;
+    private Cordination pointC;
+    private Cordination pointD;
 
+    public Cordination getPointA() {
+        return pointA;
+    }
 
-                    j++;
-                }
-                if ((s.charAt(j) == ')') && (s.charAt(j+1)==' ')){
-                    String x =  s.substring(i+2,j);
-//                        System.out.println(x);
-                    double so = Double.parseDouble(x);
-                    System.out.println(so);
-                    if (so < 0) {
-                        System.out.println("Error, the value must be positive .");
-                        return false;
-                    } else {
-                        list.add(so);
-                        i = j + 1;
-                    }
-                }
-                s = s.substring(i);
-                s= s.trim();
-                System.out.println(s);
-                String[] s1 = s.split(" ");
-                if ( s1.length == 2 ){
-                    double so1 = Double.parseDouble(s1[0]);
-                    if ( so1 > 0 && so1 <= 90){
-                        System.out.println(so1);
-                        setSightCorner(so1);
-                    }
-                    else {
-                        return false;
-                    }
-                    so1 = Double.parseDouble(s1[1]);
-                    if ( so1 > 0 && so1 <= 100){
-                        System.out.println(so1);
-                        setScope(so1);
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                else {
-                    return  false;
-                }
-            }
-        }
-        if (list.size() == 3 ){
-            setX(list.get(0));
-            setY(list.get(1));
-            setZ(list.get(2));
-            System.out.println(list.size());
-        }
-        else {
-            return false;
-        }
-        Calculate calculate = new Calculate();
-        double width = calculate.distancePointAB(room.getPointA(), room.getPointB());
-        double length = calculate.distancePointAB(room.getPointA(), room.getPointD());
-        double height = calculate.distancePointAB(room.getPointA(),room.getPointA1());
-        if ((this.getX()== length)&& (this.getY() <=  width && (this.getZ() <=height))  ||
-                ((this.getY() == width) && (this.getX()<= height) && (this.getZ() <= height))  ||
-                ((this.getX()==0) && (this.getY() <= width) && (this.getZ() <= height)) ||
-                ((this.getY() == 0) && (this.getX() <= length) && (this.getZ() <= height)) ){
-            return true;
-        } else {
-            return false;
-        }
+    public void setPointA(Cordination pointA) {
+        this.pointA = pointA;
+    }
+
+    public Cordination getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Cordination pointB) {
+        this.pointB = pointB;
+    }
+
+    public Cordination getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Cordination pointC) {
+        this.pointC = pointC;
+    }
+
+    public Cordination getPointD() {
+        return pointD;
+    }
+
+    public void setPointD(Cordination pointD) {
+        this.pointD = pointD;
+    }
+
+    @Override
+    public String toString() {
+        return "Camera{" +
+                "pointA=" + pointA +
+                ", pointB=" + pointB +
+                ", pointC=" + pointC +
+                ", pointD=" + pointD +
+                '}';
     }
 
     public int checkVisibleArea( Camera camera, Calculate obj)
@@ -130,29 +83,99 @@ public class Camera extends Cordination {
         // tinh y :
         Calculate calculate = new Calculate();
         Cordination cam = new Cordination(camera.getX(), camera.getY(), camera.getZ());
-         double edge =  (camera.getX() * Math.tan(camera.getSightCorner() / 2) ) * 2;
+        double edge =  (camera.getX() * Math.tan(camera.getSightCorner() / 2) ) * 2;
         // xet truogn hop camera dat tren OYZ
         // xet x chay tu camera
-        for(double x = camera.getX() ;  x < camera.getScope() ; x += 0.01)
+        int countPoint = 0 ;
+        int count = 0;
+        int count1 = 0 ;
+        int count2 = 0 ;
+        int count3 = 0;
+        double y = camera.getY();
+        double x = camera.getX();
+        System.out.println(" scope: "+camera.getScope());
+        System.out.println(" x: "+ x*edge);
+        for(x = camera.getX() + 0.01 ;  x < camera.getScope() ; x += 0.01)
         {
-            for(double y = camera.getY() ; y < x * edge  ; y += 0.01)
+            count1++;
+            System.out.println(" x :"+ x * edge);
+            for(y = camera.getY() ; y < x * edge  ; y += 0.01)
             {
+                System.out.println(" y" + y);
+                count2++;
                 for( double z = camera.getZ() ; z < x * edge ; z += 0.01)
                 {
+                    count3++;
                     Cordination currentPoint = new Cordination( x, y ,z);
                     if(calculate.checkPointInCamera(currentPoint, camera))
                     {
                         // tinh vector chi phuon gcua duong thang noi diem dang xet voi camera
-                        calculate.makeVectorU(cam, currentPoint);
+                        Cordination vectorU = calculate.makeVectorU(cam, currentPoint);
                         // tinh vector phap tuyen cua 5 mat phang can phai xet
                         // xet doi voi tung vat the co trong phong
                         for( int k = 1 ; k < obj.objectsList.size() ; k++) {
+                            count ++;
+                            System.out.println(" count : " + count);
                             Cordination vectorAA1D1 = calculate.makeVectorN(obj.objectsList.get(k).getPointA(),
                                     obj.objectsList.get(k).getPointA1(), obj.objectsList.get(k).getPointD1());
+                            Cordination vectorAA1B1 = calculate.makeVectorN(obj.objectsList.get(k).getPointA(),
+                                    obj.objectsList.get(k).getPointA1(), obj.objectsList.get(k).getPointB1());
+                            Cordination vectorA1B1C1 = calculate.makeVectorN(obj.objectsList.get(k).getPointA1(),
+                                    obj.objectsList.get(k).getPointB1(), obj.objectsList.get(k).getPointC1());
+                            Cordination vectorBB1C1 = calculate.makeVectorN(obj.objectsList.get(k).getPointB(),
+                                    obj.objectsList.get(k).getPointB1(), obj.objectsList.get(k).getPointC1());
+                            Cordination vectorCC1D1 = calculate.makeVectorN(obj.objectsList.get(k).getPointC(),
+                                    obj.objectsList.get(k).getPointC1(), obj.objectsList.get(k).getPointD1());
+                            // xet xem duong thang co cat phang phang khong ( cos chay tu -1 den 1)
+                            double cos1 = (vectorU.getX() * vectorAA1D1.getX() + vectorU.getY() * vectorAA1D1.getY()
+                                    + vectorU.getZ() * vectorAA1D1.getZ()) / (Math.sqrt((Math.pow(vectorU.getX(), 2)) + (Math.pow(vectorU.getY(), 2))
+                                    + (Math.pow(vectorU.getZ(), 2))) *  Math.sqrt((Math.pow(vectorAA1D1.getX(), 2)) + (Math.pow(vectorAA1D1.getY(), 2))
+                                    + (Math.pow(vectorAA1D1.getZ(), 2))));
+                            System.out.println("cos1 : "+ cos1);
+                            double cos2 = (vectorU.getX() * vectorAA1B1.getX() + vectorU.getY() * vectorAA1B1.getY()
+                                    + vectorU.getZ() * vectorAA1B1.getZ()) / (Math.sqrt((Math.pow(vectorU.getX(), 2)) + (Math.pow(vectorU.getY(), 2))
+                                    + (Math.pow(vectorU.getZ(), 2))) *  Math.sqrt((Math.pow(vectorAA1B1.getX(), 2)) + (Math.pow(vectorAA1B1.getY(), 2))
+                                    + (Math.pow(vectorAA1B1.getZ(), 2))));
+                            System.out.println("cos2 : "+ cos2);
+                            double cos3 = (vectorU.getX() * vectorA1B1C1.getX() + vectorU.getY() * vectorA1B1C1.getY()
+                                    + vectorU.getZ() * vectorA1B1C1.getZ()) / (Math.sqrt((Math.pow(vectorU.getX(), 2)) + (Math.pow(vectorU.getY(), 2))
+                                    + (Math.pow(vectorU.getZ(), 2))) *  Math.sqrt((Math.pow(vectorA1B1C1.getX(), 2)) + (Math.pow(vectorA1B1C1.getY(), 2))
+                                    + (Math.pow(vectorA1B1C1.getZ(), 2))));
+                            System.out.println("cos3 : "+ cos3);
+                            double cos4 = (vectorU.getX() * vectorBB1C1.getX() + vectorU.getY() * vectorBB1C1.getY()
+                                    + vectorU.getZ() * vectorBB1C1.getZ()) / (Math.sqrt((Math.pow(vectorU.getX(), 2)) + (Math.pow(vectorU.getY(), 2))
+                                    + (Math.pow(vectorU.getZ(), 2))) *  Math.sqrt((Math.pow(vectorBB1C1.getX(), 2)) + (Math.pow(vectorBB1C1.getY(), 2))
+                                    + (Math.pow(vectorBB1C1.getZ(), 2))));
+                            System.out.println("cos4 : "+ cos4);
+                            double cos5 = (vectorU.getX() * vectorCC1D1.getX() + vectorU.getY() * vectorCC1D1.getY()
+                                    + vectorU.getZ() * vectorCC1D1.getZ()) / (Math.sqrt((Math.pow(vectorU.getX(), 2)) + (Math.pow(vectorU.getY(), 2))
+                                    + (Math.pow(vectorU.getZ(), 2))) *  Math.sqrt((Math.pow(vectorCC1D1.getX(), 2)) + (Math.pow(vectorCC1D1.getY(), 2))
+                                    + (Math.pow(vectorCC1D1.getZ(), 2))));
+                            System.out.println("cos5 : "+ cos5);
+                            if( cos1 > -1 && cos1 < 1 || cos2 > -1 && cos2 < 1 || cos3 > -1 && cos3 < 1 || cos4 > -1 && cos4 < 1
+                                    || cos5 > -1 && cos5 < 1)
+                            {
+                                countPoint++;
+                            }
                         }
                     }
                 }
             }
         }
+        System.out.println("count1 : "+ count1);
+        System.out.println("count2 : "+ count2);
+        System.out.println("count3 : "+ count3);
+        return countPoint;
     }
+    public double ratioVisibleArea( int countPoint)
+    {
+        // double a =
+        return  1;
+    }
+    public void printParameter() {
+        System.out.println("Cordination: (" + this.getX() + ", " + this.getY() + ", " + this.getZ() + ")");
+        System.out.println("Sight corner: " + this.sightCorner);
+        System.out.println("Scope: " + this.scope);
+    }
+
 }
--- src/customer/Test.java
+++ src/customer/Test.java
@@ -1,43 +1,56 @@
 package customer;
 import object.Calculate;
+import object.Things;
+
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Calculate object = new Calculate();
-        String s;
-        Camera camera =new Camera();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the size of room : ");
-        s = input.nextLine();
-        if (object.checkRoom(s)) {
-            System.out.println("Enter number of object : ");
-            Scanner scanner = new Scanner(System.in);
-            int numberObj = scanner.nextInt();
-            if (numberObj < 0) {
-                System.out.println(" the value must be positive");
-            } else {
-                scanner.nextLine();
-                for (int i = 0; i < numberObj; i++) {
-                    System.out.println("Enter coordination of object " + (i + 1));
-                    String s1 = scanner.nextLine();
-                    if(!object.checkObject(s1, object))
-                    {
-                        System.out.println(" Invalid !");
-                    }
-                    else {
-                        // kiem tra tinh hop le cua vat the
-                        System.out.println("ok ok ok");
-                    }
-                }
-            }
-            System.out.println("Enter Cam");
-            String cam= input.nextLine();
-            System.out.println(camera.checkCamare(object.objectsList.get(0), cam));
-
-        }
-        else {
-            System.out.println("Invalid! Try again.");
-        }
+        //Cho nay nho thay duong dan nhe :v
+        String fileName = "D:\\13.txt";
+        Things thing = new Things();
+        thing.readInputFile(fileName);
     }
 }
+
+//        Calculate object = new Calculate();
+//        String s;
+//        Camera camera =new Camera();
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the size of room : ");
+//        s = input.nextLine();
+//        if (object.checkRoom(s)) {
+//            System.out.println("Enter number of object : ");
+//            Scanner scanner = new Scanner(System.in);
+//            int numberObj = scanner.nextInt();
+//            if (numberObj < 0) {
+//                System.out.println(" the value must be positive");
+//            } else {
+//                scanner.nextLine();
+//                for (int i = 0; i < numberObj; i++) {
+//                    System.out.println("Enter coordination of object " + (i + 1));
+//                    String s1 = scanner.nextLine();
+//                    if(!object.checkObject(s1, object))
+//                    {
+//                        System.out.println(" Invalid !");
+//                    }
+//                    else {
+//                        // kiem tra tinh hop le cua vat the
+//                        System.out.println("ok ok ok");
+//                    }
+//                }
+//            }
+//            System.out.println("Enter Cam");
+//            String cam= input.nextLine();
+//            if(camera.checkCamare(object.objectsList.get(0), cam))
+//            {
+//                System.out.println(" vao check diem");
+//                int check = camera.checkVisibleArea(camera, object);
+//                System.out.println("check " +check);
+//            }
+//
+//        }
+//        else {
+//            System.out.println("Invalid! Try again.");
+//        }
+//    }
